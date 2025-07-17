b1c6846f746773f1348ca0221d1bfa270f40c936
perfectplaytv <graficahome@zoho.com>
2025-07-17 19:33:22 -0300

    confirmar

---
+++ src/pages/AdminRadio.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AdminRadio() {
+  return (
+    <div>
+      <h1>Rádio Web</h1>
+      <p>Página de gerenciamento da rádio web.</p>
+    </div>
+  );
+} 
