f7c61382b8c60ae0439949513dcabac656e98d0a
perfectplaytv <graficahome@zoho.com>
2025-07-17 19:33:36 -0300

    confirmar

---
+++ src/pages/AdminGames.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AdminGames() {
+  return (
+    <div>
+      <h1>Gamificação</h1>
+      <p>Página de gerenciamento de gamificação.</p>
+    </div>
+  );
+} 
