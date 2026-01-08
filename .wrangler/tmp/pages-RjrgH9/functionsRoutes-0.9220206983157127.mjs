66c06827e8eef54d12f291a6597aa759371fcfee
perfectplaytv <graficahome@zoho.com>
2026-01-08 00:52:18 -0300

    confirmar

---
+++ .wrangler/tmp/pages-RjrgH9/functionsRoutes-0.9220206983157127.mjs
@@ -0,0 +1,35 @@
+import { onRequestPost as __api_auth_login_ts_onRequestPost } from "D:\\boot-flow\\functions\\api\\auth\\login.ts"
+import { onRequestPost as __api_auth_register_ts_onRequestPost } from "D:\\boot-flow\\functions\\api\\auth\\register.ts"
+import { onRequestGet as __api_users_index_ts_onRequestGet } from "D:\\boot-flow\\functions\\api\\users\\index.ts"
+import { onRequestPost as __api_users_index_ts_onRequestPost } from "D:\\boot-flow\\functions\\api\\users\\index.ts"
+
+export const routes = [
+    {
+      routePath: "/api/auth/login",
+      mountPath: "/api/auth",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_auth_login_ts_onRequestPost],
+    },
+  {
+      routePath: "/api/auth/register",
+      mountPath: "/api/auth",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_auth_register_ts_onRequestPost],
+    },
+  {
+      routePath: "/api/users",
+      mountPath: "/api/users",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_users_index_ts_onRequestGet],
+    },
+  {
+      routePath: "/api/users",
+      mountPath: "/api/users",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_users_index_ts_onRequestPost],
+    },
+  ]
