9a908cb66c5157d71a9f2f9d7c35daa052032679
perfectplaytv <graficahome@zoho.com>
2026-01-11 05:12:37 -0300

    confirmar

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -10,14 +10,14 @@ interface UserContextType {
 }
 
 // URL do avatar padrão
-const defaultAvatar = 'https://source.boringavatars.com/beam/120/Admin?colors=7e22ce,a855f7,6d28d9,e9d5ff';
+const defaultAvatar = 'https://ui-avatars.com/api/?name=Admin&background=7e22ce&color=fff&size=128';
 
 // Cria o contexto com um valor padrão
 const UserContext = createContext<UserContextType>({
   userName: 'Admin',
   userEmail: 'admin@email.com',
   avatar: defaultAvatar,
-  setAvatar: () => {}, // função vazia como padrão
+  setAvatar: () => { }, // função vazia como padrão
 });
 
 // Hook customizado para facilitar o uso do contexto
@@ -26,7 +26,7 @@ export const useUser = () => useContext(UserContext);
 // Componente Provedor que vai englobar a aplicação
 export const UserProvider = ({ children }: { children: ReactNode }) => {
   const [avatar, setAvatar] = useState<string>(defaultAvatar);
-  
+
   // Em um aplicativo real, estes dados viriam de uma API após o login
   const [userName, setUserName] = useState('Admin');
   const [userEmail, setUserEmail] = useState('admin@email.com');
