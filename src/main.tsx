950404f54178a227fe313f95573dabf67711ba9e
perfectplaytv <graficahome@zoho.com>
2026-01-12 12:59:00 -0300

    confirmar

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Build timestamp: 2026-01-12T12:55:00
 createRoot(document.getElementById("root")!).render(<App />);
